473963f7a2f7d44eb724921124f4f272eb807dbb
Pascual Perales <pascualdelangel01@gmail.com>
2025-01-18 19:33:31 -0400

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,11 @@
-pip install -r requirements.txt
-python manage.py collectstatic --noinput
+#!/bin/bash
+# Build the project
+echo "Building the project..."
+python -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
+
+echo "Collect Static..."
+python manage.py collectstatic --noinput --clear
