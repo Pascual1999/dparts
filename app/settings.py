b754d13343d615d12e3c88e950c1159fb57da49f
Pascual Perales <pascualdelangel01@gmail.com>
2025-02-08 21:50:54 -0400

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ["127.0.0.1", ".onrender.com", ".now.sh",
     "localhost", "dparts-production.up.railway.app"]
